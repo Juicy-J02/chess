f53af8b9e07384901255c18cf96d81eba8a3d29a
Jackson <jrgray609@gmail.com>
2025-11-13 00:36:47 -0700

    Chess Phase 5: PostLogin

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,7 +4,7 @@ import ui.PreloginUI;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("♕ 240 Chess Client");
-        ServerFacade sever = new ServerFacade("http://localhost:8080");
-        new PreloginUI(sever).run();
+        ServerFacade server = new ServerFacade("http://localhost:8080");
+        new PreloginUI(server).run();
     }
 }
