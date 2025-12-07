bcf72cd8f1f99b34b5ad3cc34601a31eb45e2ae5
Jackson <jrgray609@gmail.com>
2025-12-07 12:44:35 -0700

    Chess Phase 6: WS

--- client/src/main/java/server/WebsocketCommunicator.java
+++ client/src/main/java/server/WebsocketCommunicator.java
@@ -1,54 +1,61 @@
 package server;
 
+import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;
+import java.net.URISyntaxException;
 import java.net.http.WebSocket;
-import java.net.http.WebSocket.Listener;
-import java.util.concurrent.CompletionStage;
+
 import com.google.gson.Gson;
+import jakarta.websocket.*;
+import ui.GameplayUI;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ServerMessage;
 
-public class WebsocketCommunicator {
+public class WebsocketCommunicator extends Endpoint {
 
-    private WebSocket webSocket;
-    private final Gson gson = new Gson();
+    Session session;
 
-    public WebsocketCommunicator(String serverUrl) {
-        HttpClient client = HttpClient.newHttpClient();
+    public WebsocketCommunicator(String url) {
+        try {
+            url = url.replace("http", "ws");
+            URI uri = new URI(url + "/ws");
 
-        client.newWebSocketBuilder()
-                .buildAsync(URI.create(serverUrl.replace("http", "ws") + "/ws"), new Listener() {
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, uri);
 
-                    @Override
-                    public void onOpen(WebSocket ws) {
-                        System.out.println("Connected to WebSocket server!");
-                        WebsocketCommunicator.this.webSocket = ws;
-                        Listener.super.onOpen(ws);
-                    }
-
-                    @Override
-                    public CompletionStage<?> onText(WebSocket ws, CharSequence data, boolean last) {
-                        System.out.println("Received message: " + data);
-                        return Listener.super.onText(ws, data, last);
-                    }
-
-                    @Override
-                    public CompletionStage<?> onClose(WebSocket ws, int statusCode, String reason) {
-                        System.out.println("WebSocket closed: " + statusCode + " - " + reason);
-                        return Listener.super.onClose(ws, statusCode, reason);
-                    }
-
-                    @Override
-                    public void onError(WebSocket ws, Throwable error) {
-                        System.err.println("WebSocket error: " + error.getMessage());
-                        Listener.super.onError(ws, error);
-                    }
-                });
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    handleMessage(message);
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            System.out.println("Websocket error" + ex.getMessage());
+        }
     }
 
-    public void sendCommand(Object command) {
-        if (webSocket != null) {
-            String json = gson.toJson(command);
-            webSocket.sendText(json, true);
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    private void handleMessage(String message) {
+        ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+        if (serverMessage.getServerMessageType() == ServerMessage.ServerMessageType.LOAD_GAME) {
+            LoadGameMessage loadGameMessage = new Gson().fromJson(message, LoadGameMessage.class);
         }
+        else if (serverMessage.getServerMessageType() == ServerMessage.ServerMessageType.ERROR) {
+            ErrorMessage errorMessage = new Gson().fromJson(message, ErrorMessage.class);
+            System.out.println("Error: " + errorMessage.getError());
+        }
+        else if (serverMessage.getServerMessageType() == ServerMessage.ServerMessageType.NOTIFICATION) {
+            NotificationMessage notificationMessage = new Gson().fromJson(message, NotificationMessage.class);
+            System.out.println(notificationMessage.getNotification());
+        }
+    }
+
+    public void sendMessage(String json) {
+        this.session.getAsyncRemote().sendText(json);
     }
 }
